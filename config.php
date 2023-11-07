9f11a1fa8c172f8916c8dd04736adba79160e0c4
Rahuldeshmukhsoulsoft <148740675+Rahuldeshmukhsoulsoft@users.noreply.github.com>
2023-11-07 12:06:41 +0530

    Bill setup

---
+++ config.php
@@ -0,0 +1,15 @@
+
+
+<?php
+// 
+ $con=mysqli_connect("localhost","root","","curvedent")or die(mysqli_error($con));
+//  $con=mysqli_connect("localhost","u311423116_root","XK;ded5>","u311423116_curvbilling")or die(mysqli_error($con));
+
+
+
+ // For production
+
+//  $con=mysqli_connect("localhost","gvsoft_root","gvsoft@2023","gvsoft_rgb_oos")or die(mysqli_error($con));
+
+?>
+
